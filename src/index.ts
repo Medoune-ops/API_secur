90b4ec7795d8832b3ef241a9c6bd6e3a17f98828
Medoune seck <smedoune16@gmail.com>
2026-06-01 17:00:20 +0000

    fix: use process.env.PORT for Render compatibility

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use("/users", userRoutes);
 // GESTION DES ERREURS  
 app.use(errorHandler);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 AppDataSource.initialize()
     .then(() => {
